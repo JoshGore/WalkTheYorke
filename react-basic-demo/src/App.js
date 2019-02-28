758aca1eb3ad10100f811050a5a6d78ef408cb17
Johsua Gore (Desktop) <Joshua.Gore8@gmail.com>
2019-02-28 13:04:50 +1030

    resolves #3 #1 improved styling including casing and zoom depentant and hover style changes

--- react-basic-demo/src/App.js
+++ react-basic-demo/src/App.js
@@ -1,117 +1,195 @@
 import React, { Component } from 'react';
 import './App.css';
 // import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-import ReactMapboxGl, { Layer, GeoJSONLayer } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, GeoJSONLayer, Source } from "react-mapbox-gl";
 import MapboxGL from "mapbox-gl";
 
 const Map = ReactMapboxGl({
     accessToken: "pk.eyJ1Ijoiam9zaGciLCJhIjoiTFBBaE1JOCJ9.-BaGpeSYz4yPrpxh1eqT2A",
 })
 
-const geojson = require('./data/walktheyorke_naturemaps_stages.geojson')
+// const geojson = require('./data/walktheyorke_naturemaps_stages.geojson')
+const WTY_SOURCE = {
+    "type": "geojson",
+    "data": "/data/walktheyorke_naturemaps_stages.geojson"
+};
+
+const TRAIL_LINE_COLOR =  
+    [
+        "case",
+        [
+            "match",
+            ["get", "TRAILTYPE"],
+            ["", "WALKING"],
+            true,
+            false
+        ],
+        "hsl(33, 100%, 64%)",
+        [
+            "match",
+            ["get", "TRAILTYPE"],
+            ["", "CYCLING"],
+            true,
+            false
+        ],
+        "hsl(0, 100%, 69%)",
+        [
+            "match",
+            ["get", "TRAILTYPE"],
+            ["", "SU:WALK,BIKE"],
+            true,
+            false
+        ],
+        "hsl(82, 100%, 41%)",
+        "hsl(46, 98%, 30%)"
+    ]
+;
+
+const TRAIL_PAINT = {
+    "line-color": TRAIL_LINE_COLOR,
+    "line-width": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        4,
+        1,
+        22,
+        5
+    ]
+}
+const TRAIL_CASE_PAINT = {
+    "line-color": "white",
+    "line-width": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        4,
+        2,
+        22,
+        10
+    ]
+}
+
+const TRAIL_HIGHLIGHT_PAINT = {
+    "line-color": TRAIL_LINE_COLOR,
+    "line-width": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        4,
+        2,
+        22,
+        10
+    ]
+}
+
+const TRAIL_HIGHLIGHT_CASE_PAINT = {
+    "line-color": "white",
+    "line-width": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        4,
+        4,
+        22,
+        20
+    ]
+}
 
 class App extends Component {
     onMapLoad = (map) => {
-        map.addControl(new MapboxGL.GeolocateControl());
+        map.setMaxBounds (map.getBounds());
+        var geolocate = new MapboxGL.GeolocateControl({
+            trackUserLocation: true,
+            fitBoundsOptions: {
+                maxZoom: map.getZoom()
+            }
+        });
+        map.addControl(geolocate);
+        /*
+        setTimeout(() => {
+            geolocate.trigger(); 
+        }, 100);
+        */
     }
-    onMouseMove(map, evt) {
-        // console.log(map.queryRenderedFeatures(evt.point));
+    trailLineMouseMove(evt) {
+        var map = evt.target
+        var feature = map.queryRenderedFeatures(evt.point)[0];
+        map.setFilter('trail_lines_highlight', ['in', 'STAGE', feature.properties.STAGE]);
+        map.setFilter('trail_lines_highlight_case', ['in', 'STAGE', feature.properties.STAGE]);
+        map.getCanvas().style.cursor = 'pointer';
     }
-    wtyLineMouseMove(evt) {
-        var feature = evt.target.queryRenderedFeatures(evt.point)[0];
-        var related = evt.target.querySourceFeatures('WTY', {
-            sourceLayer: 'WTY-line',
-            filter: ['in', 'STAGE', feature.properties.STAGE]
-        })
-        console.log(related);
+    trailLineMouseLeave(evt) {
+        var map = evt.target
+        var feature = map.queryRenderedFeatures(evt.point)[0];
+        map.setFilter('trail_lines_highlight', ['in', 'STAGE', ""]);
+        map.setFilter('trail_lines_highlight_case', ['in', 'STAGE', ""]);
+        map.getCanvas().style.cursor = '';
     }
     render() {
         return (
             <Map
             fitBounds={[[136.585109, -35.314486],[138.366868, -33.990990]]}
-            // maxBounds={[[136.585109, -35.314486],[138.366868, -33.990990]]} 
             onStyleLoad={this.onMapLoad}
-            // style="mapbox://styles/joshg/cjsmplt1d0cgv1fqmdzwntor7"
             style="mapbox://styles/mapbox/outdoors-v11"
-            onMouseMove={this.onMouseMove}
             containerStyle={{
                 height: "100vh",
                 width: "100vw"
             }}>
-            <GeoJSONLayer 
-            // name = "WTY"
-            id = "WTY"
-            data = { geojson }
-            lineOnMouseMove = {this.wtyLineMouseMove}
-            linePaint = {
-                { "line-color": [
-                    "case",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "WALKING"],
-                        true,
-                        false
-                    ],
-                    "hsl(33, 98%, 56%)",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "CYCLING"],
-                        true,
-                        false
-                    ],
-                    "hsl(0, 94%, 48%)",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "SU:WALK,BIKE"],
-                        true,
-                        false
-                    ],
-                    "hsl(131, 96%, 34%)",
-                    "hsl(33, 0%, 0%)"
-                ] }
-            }
-            />
-            {/*
-            <Layer>
-            id="WTY-Highlight"
-            type="line"
-            linePaint = {
-                { "line-color": [
-                    "case",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "WALKING"],
-                        true,
-                        false
-                    ],
-                    "hsl(33, 98%, 56%)",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "CYCLING"],
-                        true,
-                        false
-                    ],
-                    "hsl(0, 94%, 48%)",
-                    [
-                        "match",
-                        ["get", "TRAILTYPE"],
-                        ["", "SU:WALK,BIKE"],
-                        true,
-                        false
-                    ],
-                    "hsl(131, 96%, 34%)",
-                    "hsl(33, 0%, 0%)"
-                ],
-                "line-width": 2}
-            }
-            
-            </Layer>
-            */}
+            <Source id="trail_lines" geoJsonSource={WTY_SOURCE} />
+            <Layer 
+                id = "trail_lines_case"
+                type = "line"
+                sourceId = "trail_lines"
+                layout = {{
+                    "line-join": "round",
+                    "line-cap": "round"
+                }}
+                paint = {TRAIL_CASE_PAINT}
+            ></Layer>
+            <Layer 
+                id = "trail_lines"
+                type = "line"
+                sourceId = "trail_lines"
+                layout = {{
+                    "line-join": "round",
+                    "line-cap": "round"
+                }}
+                paint = {TRAIL_PAINT}
+            ></Layer>
+            <Layer 
+                id = "trail_lines_highlight_case"
+                type = "line"
+                sourceId = "trail_lines"
+                layout = {{
+                    "line-join": "round",
+                    "line-cap": "round"
+                }}
+                paint = {TRAIL_HIGHLIGHT_CASE_PAINT}
+                filter = {["in", "STAGE", ""]}
+            ></Layer>
+            <Layer 
+                id = "trail_lines_highlight"
+                type = "line"
+                sourceId = "trail_lines"
+                layout = {{
+                    "line-join": "round",
+                    "line-cap": "round"
+                }}
+                paint = {TRAIL_HIGHLIGHT_PAINT}
+                filter = {["in", "STAGE", ""]}
+            ></Layer>
+            <Layer 
+                id = "trail_lines_target"
+                type = "line"
+                sourceId = "trail_lines"
+                onMouseMove = {this.trailLineMouseMove}
+                onMouseLeave = {this.trailLineMouseLeave}
+                paint = {{
+                    "line-width": 40,
+                    "line-opacity": 0
+                }}
+            ></Layer>
             </Map>
         );
     }
